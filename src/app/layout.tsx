d0c995ab5b6b22bca09633afad1278ee9180efa9
Andra Indra <indraandra545@gmail.com>
2026-03-05 20:33:26 +0000

    masih ada

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,8 +2,12 @@ import type {Metadata} from 'next';
 import './globals.css';
 
 export const metadata: Metadata = {
-  title: '',
-  description: '',
+  title: 'Platform Edukasi',
+  description: 'Belajar berbagai keahlian baru secara sistematis.',
+  icons: {
+    icon: [],
+    apple: [],
+  },
 };
 
 export default function RootLayout({
