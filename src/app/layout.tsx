42fb816f8b46b09f6e9429220f22162db7f81ef8
Andra Indra <indraandra545@gmail.com>
2026-03-05 21:02:52 +0000

    masih sama

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,12 +7,12 @@ export const metadata: Metadata = {
   icons: {
     icon: [
       {
-        url: 'https://spmb.smkwikrama.sch.id/assets/landing%20page/images/logo.png?v=2',
-        href: 'https://spmb.smkwikrama.sch.id/assets/landing%20page/images/logo.png?v=2',
+        url: 'https://spmb.smkwikrama.sch.id/assets/landing%20page/images/logo.png?v=3',
+        href: 'https://spmb.smkwikrama.sch.id/assets/landing%20page/images/logo.png?v=3',
       },
     ],
-    shortcut: 'https://spmb.smkwikrama.sch.id/assets/landing%20page/images/logo.png?v=2',
-    apple: 'https://spmb.smkwikrama.sch.id/assets/landing%20page/images/logo.png?v=2',
+    shortcut: 'https://spmb.smkwikrama.sch.id/assets/landing%20page/images/logo.png?v=3',
+    apple: 'https://spmb.smkwikrama.sch.id/assets/landing%20page/images/logo.png?v=3',
   },
 };
 
@@ -28,9 +28,9 @@ export default function RootLayout({
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&family=Inter:wght@300;400;500;600&display=swap" rel="stylesheet" />
         <meta name="referrer" content="no-referrer" />
-        <link rel="icon" type="image/png" href="https://spmb.smkwikrama.sch.id/assets/landing%20page/images/logo.png?v=2" />
-        <link rel="shortcut icon" type="image/png" href="https://spmb.smkwikrama.sch.id/assets/landing%20page/images/logo.png?v=2" />
-        <link rel="apple-touch-icon" href="https://spmb.smkwikrama.sch.id/assets/landing%20page/images/logo.png?v=2" />
+        <link rel="icon" type="image/png" href="https://spmb.smkwikrama.sch.id/assets/landing%20page/images/logo.png?v=3" />
+        <link rel="shortcut icon" type="image/png" href="https://spmb.smkwikrama.sch.id/assets/landing%20page/images/logo.png?v=3" />
+        <link rel="apple-touch-icon" href="https://spmb.smkwikrama.sch.id/assets/landing%20page/images/logo.png?v=3" />
       </head>
       <body className="font-body antialiased selection:bg-primary selection:text-primary-foreground">
         {children}
